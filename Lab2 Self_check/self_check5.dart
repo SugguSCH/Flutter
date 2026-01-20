10316a024ba03132dbc88f33632d612c585f01a1
Chanasorn Chirapongsathon <141487719+SugguSCH@users.noreply.github.com>
2026-01-20 21:15:23 +0700

    Operation in dart

---
+++ Lab2 Self_check/self_check5.dart
@@ -0,0 +1,18 @@
+// - x + y
+// - y/x
+// - y++
+// - x > y
+// - x != y
+// - x+=y
+// - x--
+void main() {
+  int x = 9;
+  int y = 2;
+  print(x+y);
+  print(y/x);
+  print(y++);
+  print(x > y);
+  print(x != y);
+  print(x += y);
+  print(x--);
+}
