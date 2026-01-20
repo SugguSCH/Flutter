a5b10d98451658e99c83a4f898af94cb49c8cae6
Chanasorn Chirapongsathon <141487719+SugguSCH@users.noreply.github.com>
2026-01-20 21:16:43 +0700

    simple  condition in dart

---
+++ Lab2 Self_check/self_check6_7.dart
@@ -0,0 +1,12 @@
+void main() {
+  int score = 80;
+  if(score >= 80){
+    print("Outstanding my man");
+  } else if (score >= 60){
+    print("Good my man");
+  } else if (score >= 40){
+    print("mhh");
+  } else {
+    print("Hell nooooooooo.");
+  }
+}
