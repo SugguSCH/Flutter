f845627f14972e3923455876438234302b886296
Chanasorn Chirapongsathon <141487719+SugguSCH@users.noreply.github.com>
2026-01-20 21:17:15 +0700

    transitive condition

---
+++ Lab2 Self_check/self_check7.dart
@@ -0,0 +1,20 @@
+void main() {
+  int a = 4;
+  int b = 12;
+  int c = 8;
+  if (a > b){
+    if (a > c) {
+      print("a is the greatest");
+    } else {
+      print("c is the greatest");
+    }
+  } else if (b > c) {
+    if (b > c){ 
+      print("b is the greatest");
+    } else {
+      print("c is the greatest");
+    }
+  } else {
+      print("b is the greatest");
+  }
+}
